08edd323ad2ab834f0dfed3abb5a5214e1f0768d
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-04-04 01:18:59 +0100

    create project with vite, delete unnecessary files, add project asset and navbar.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import  Navbar  from './components/sharedlayouts/Navbar';
+
+function App() {
+  return (
+    <div>
+       <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/sharedlayouts/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import "../../styles/Navbar.css";
+import profileImg from "../../assets/image 4.png";
+
+const Navbar: React.FC = () => {
+  return (
+    <header>
+      <div className="navbar">
+        <div className="navbar__searchbar">
+          <form className="searchbar">
+            <div className="form-details">
+              <input type="text" placeholder="Search for anything" />
+              <button title="Search">
+                <AiOutlineSearch /> 
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="navbar__links">
+          <ul>
+            <li>
+              <p className="docs">Docs</p>
+            </li>
+            <li>
+              <p className="bell">
+                <IoIosNotificationsOutline />
+              </p>
+            </li>
+            <li>
+              <img
+                className="img__container"
+                src={profileImg}
+                alt="profile img"
+              />
+              <p className="username">Adedeji</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
